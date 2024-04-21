75d90809107697aaa7be6d06604b0d2da3dfdb5f
laabiiii <164504056+laabiiii@users.noreply.github.com>
2024-04-21 19:48:45 +0500

    creating wordcounter

---
+++ index.js
@@ -0,0 +1,11 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answers = await inquirer.prompt([{
+        name: "sentence",
+        type: "input",
+        message: "Enter your sentence to count the word :"
+    }
+]);
+const words = answers.sentence.trim().split(" ");
+console.log(words);
+console.log(`your sentence words count is ${words.length}`);
